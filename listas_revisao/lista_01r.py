af7bce818459e1c6c3435820f644f794994fb89b
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-05-06 14:01:47 +0000

    "listas"

--- listas_revisao/lista_01r.py
+++ listas_revisao/lista_01r.py
@@ -1,3 +1,22 @@
 #4
 
-()
+print("Digite o primeiro horário no formato hh:mm")
+h_1 = input()
+print("Digite o segundo horário no formato hh:mm")
+h_2 = input()
+
+hora1 = list(h_1)
+hora2 = list(h_2)
+
+i = 0
+soma = 0
+for i in range(5):
+    if i != 2:
+        if i == 3 and soma > 6:
+            soma = int(hora1[1]) + int(hora2[1]) + 1
+        else:
+            soma = int(hora1[i]) + int(hora2[i])
+        print(soma)
+
+    
+print(f"Total de horas:{soma}")
