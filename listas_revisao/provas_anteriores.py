3f3b0e7a5ea93b7fef3cf32bc2f650d33edacf5b
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-05-08 13:05:22 +0000

    questoes provas anteriores

---
+++ listas_revisao/provas_anteriores.py
@@ -0,0 +1,35 @@
+#11
+#-10
+#12.5
+#4
+#2.5
+
+#1
+print(1 - 2 + 3 * 4)
+print(1 * 2 - 3 * 4)
+print(1 / 2 + 3 * 4)
+print(1 // 2 * 3 + 4)
+print(1 + 2 * 3 / 4)
+
+#2
+#n = input("Digite três números separados por espaços:\n")
+#nums = n.split()
+#soma = 0
+#for i in range(3):
+#  if int(nums[i]) % 2 == 0:
+#    soma = soma + int(nums[i])
+#print(f"Soma dos pares: {soma}")
+
+#3
+
+#f = input("Escreva uma frase:\n")
+#frase = list(f)
+#frasen = []
+#for i in range(len(frase)):
+#    if i % 2 == 0 or i == 0:
+#      frasen.append(frase[i])
+#print(frasen)
+
+#4
+
+#
