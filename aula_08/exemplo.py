e9ef688be0d81771c168b898e6ab68757baeac3c
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-05-15 13:23:05 +0000

    UI e ValueError

---
+++ aula_08/exemplo.py
@@ -0,0 +1,28 @@
+class Triangulo:
+    def __init__(self):
+        self.b = 0
+        self.h = 0
+    def calc_area(self):
+        return self.b * self.h / 2
+    
+class UI:
+    @staticmethod
+    def menu():
+        op = int(input("Escolha uma opção: 1 - Triangulo | 2 - Fim \n" ))
+        return op
+    @staticmethod
+    def main():
+        op = 0
+        while op != 2:
+            op = UI.menu()
+            if op == 1: UI.triangulo()
+    @staticmethod
+    def triangulo():
+        x = Triangulo()
+        x.b = int(input("Informe o valor da base:"))
+        if x.b < 0: raise ValueError("A base deve ser positiva")
+        x.h = int(input("Informe o valor da altura:"))
+        if x.h < 0: raise ValueError("A altura deve ser positiva")
+        print(f"O triângulo de base {x.b} e altura {x.h} tem área {x.calc_area()}")
+
+UI.main()
