97d1919314df9a748b5cf2af481acbe9e41d425c
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-05-27 14:07:23 +0000

    classe conta

--- aula_09/lista_encapsulamento.py
+++ aula_09/lista_encapsulamento.py
@@ -1,17 +1,35 @@
-class Circulo:
+class Conta:
     def __init__(self):
-        self.__r = 0
-        self.__pi = 3.14
-    def calc_area(self):
-        return self.__r * (self.__pi**2)
-    def calc_circunferencia(self):
-        return (2*self.__r) * self.__pi
-    def get_raio(self):
-        return self.__r
-
-
+        self.__titular = ""
+        self.__numero = ""
+        self.__saldo = 0.0
+    def set_titular(self,t):
+        if t == "": raise ValueError()
+        self.__titular = t
+    def get_titular(self):
+        return self.__titular
+    def set_numero(self,n):
+        if n == "": raise ValueError()
+        self.__numero = n
+    def get_numero(self):
+        return self.__numero
+    def get_saldo(self):
+        return self.__saldo
+    def depositar(self,v):
+        if v < 0: raise ValueError()
+        self.__saldo += v
+    def sacar(self,v):
+        if v<0: raise ValueError()
+        if v>self.__saldo:raise ValueError()
+        self.__saldo -= v
 
 class UI:
     @staticmethod
     def main():
+        x = Conta()
+        x.set_titular(input("DIGITE SEU NOME: "))
+        x.set_numero(input("DIGITE SEU NUMERO: "))
+        x.depositar(float(input("DEPOSITE UM VALOR: ")))
+        print(x.get_saldo())
 
+UI.main()
